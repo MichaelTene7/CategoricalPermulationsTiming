72c0d1c494a644a89bf140045089deab1a68561d
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2023-10-27 19:39:39 -0400

    update comparison table, make category tables, make plotting function, start category comparissions

--- Src/Loc/Analysis.R
+++ Src/Loc/Analysis.R
@@ -8,13 +8,20 @@ ERDTimes = readRDS("Output/Hiller/CategoricalPermulationsHillerTimesERRelax0.rds
 ERPhenotypes = readRDS("Output/Hiller/CategoricalPermulationsTimingHillerPhenotypesERRelax0.rds")
 
 
+SYM20Times = readRDS("Output/Hiller/CategoricalPermulationsHillerTimesSYMRelax0.2.rds")
+SYM20Phenotypes = readRDS("Output/Hiller/CategoricalPermulationsTimingHillerPhenotypesSYMRelax0.2.rds")
+
 # -- set run parameters -- 
-inUsePhenotypes = SYMPhenotypes
-inUseTimes = SYMTimes
+inUsePhenotypes = SYM20Phenotypes
+inUseTimes = SYM20Times
 comboletters = c("M", "G")
 
 
+
+
+
 # -- organize the data into a dataframe ---
+{
 phenotypes = data.frame(names(inUsePhenotypes))
 names(phenotypes) = "phenotypeName"
 for(i in 1:nrow(phenotypes)){
@@ -40,6 +47,7 @@ for(i in 1:length(uniquePhens)){
     compareData$speciesNum[which(compareData$phen == currentPhenotype)] = phenotypes$length[phenotypeNumber]
   }
 }
+compareData$speciesNum = as.numeric(compareData$speciesNum)
 
 compareData$categoryNumber = NA
 for(i in 1:nrow(compareData)){
@@ -53,8 +61,184 @@ for(i in 1:length(comboletters)){
   compareData$combo[grep(comboletters[i], compareData$phen)] = T
 }
 
+compareData$categoryChar = as.character(compareData$categoryNumber)
+}
+# -- Make subsets of the datafor each category number --- 
+{
+twoCategory = compareData[compareData$categoryNumber ==2, ]
+threeCategory = compareData[compareData$categoryNumber ==3, ]
+fourCategory = compareData[compareData$categoryNumber ==4, ]
+fiveCategory = compareData[compareData$categoryNumber ==5, ]
+sixCategory = compareData[compareData$categoryNumber ==6, ]
+
+zscores = function(dataVector){
+  zscores = (dataVector - mean(dataVector)) / sd (dataVector)
+  zscores
+}
+
+twoCategory$zScore = zscores(twoCategory$time)
+threeCategory$zScore = zscores(threeCategory$time)
+fourCategory$zScore = zscores(fourCategory$time)
+fiveCategory$zScore = zscores(fiveCategory$time)
+sixCategory$zScore = zscores(sixCategory$time)
+}
+
+# -- remove outliers from each set -- 
+allCategory = compareData
+
+getOutlierPhenotypes = function(data, outlierCutoff){
+  outlierPhens = character()
+  uniquePhens = unique(data$phen)
+  for(i in 1:length(uniquePhens)){
+    currentPhen = uniquePhens[i]
+    currentRows = data[which(data$phen == currentPhen),]
+    averageZ = mean(currentRows$zScore)
+    if(averageZ > outlierCutoff){
+      message(paste( "Phenotype", currentPhen, "is an outlier phenotype, average zScore of", averageZ))
+      outlierPhens = append(outlierPhens, currentPhen)
+    }
+  }
+  if(length(outlierPhens) == 0){
+    message("No outlier phenotypes")
+  }else{
+    message("Outliers:", paste(outlierPhens))
+  }
+  outlierPhens
+}
+
+outlierCutoff = 2
+
+twoCategoryOutlier = getOutlierPhenotypes(twoCategory, outlierCutoff)
+threeCategoryOutlier = getOutlierPhenotypes(threeCategory, outlierCutoff)
+fourCategoryOutlier = getOutlierPhenotypes(fourCategory, outlierCutoff)
+fiveCategoryOutlier = getOutlierPhenotypes(fiveCategory, outlierCutoff)
+sixCategoryOutlier = getOutlierPhenotypes(sixCategory, outlierCutoff)
+
+#manually add any remaining outliers
+#fiveCategoryOutlier = append(fiveCategoryOutlier, "AOHIM")
+
+twoCategoryNoOutliers = subset(twoCategory, !phen %in% twoCategoryOutlier)
+threeCategoryNoOutliers = subset(threeCategory, !phen %in% threeCategoryOutlier)
+fourCategoryNoOutliers = subset(fourCategory, !phen %in% fourCategoryOutlier)
+fiveCategoryNoOutliers = subset(fiveCategory, !phen %in% fiveCategoryOutlier)
+sixCategoryNoOutliers = subset(sixCategory, !phen %in% sixCategoryOutlier)
+
+allCategoryNoOutliers = rbind(twoCategoryNoOutliers, threeCategoryNoOutliers, fourCategoryNoOutliers, fiveCategoryNoOutliers, sixCategoryNoOutliers)
+
+
+
 # --- end organization of data ---
-dataSet = compareData
+
+
+
+# -- plot the data -- 
+plotDataCompare = function(dataSet, xAxis = "speciesNum", yAxis = "time", colorVar = "phen", modelType = "lm"){
+  dataName = deparse(substitute(dataSet))
+  message(dataName)
+  linearModel = lm(paste(yAxis, "~", xAxis), data = dataSet)
+  plot1 = ggplot(dataSet, aes_string(x = xAxis, y = yAxis)) +
+    geom_point(aes_string(color = colorVar))+
+    labs(title = dataName)+
+    geom_smooth(method = "lm", show.legend = F)+
+    annotate("text",
+      x = min(dataSet[,xAxis]+ ((max(dataSet[,xAxis]) - min(dataSet[,xAxis]))/4)), y = max(dataSet[, yAxis]), 
+      label = paste(
+        "y =", 
+        round(coef(linearModel)[2], 2), 
+        "x +", 
+        round(coef(linearModel)[1], 2), 
+        "   R^2 = ", 
+        format(summary(linearModel)$r.squared, digits = 3)
+        )
+      )
+  print(plot1)
+
+  dataNoOutliersName = paste(dataName, "NoOutliers", sep='')
+  dataNoOutliers = get(dataNoOutliersName)
+  if(!nrow(dataSet) == nrow(dataNoOutliers)){
+    plot2 = ggplot(dataNoOutliers, aes_string(x = xAxis, y = yAxis)) +
+      geom_point(aes_string(color = colorVar))+
+      labs(title = dataNoOutliersName)+
+      geom_smooth(method = "lm", show.legend = F)+
+      annotate("text",
+               x = min(dataSet[,xAxis]+ ((max(dataSet[,xAxis]) - min(dataSet[,xAxis]))/4)), y = max(dataNoOutliers[, yAxis]), 
+               label = paste(
+                 "y =", 
+                 round(coef(linearModel)[2], 2), 
+                 "x +", 
+                 round(coef(linearModel)[1], 2), 
+                 "   R^2 = ", 
+                 format(summary(linearModel)$r.squared, digits = 3)
+               )
+      )
+    
+    
+    print(plot2)
+  }else{
+    message("Dataset has no outliers.")
+  }
+}
+# -- Species number effect --
+plotDataCompare(twoCategory)
+plotDataCompare(threeCategory)
+plotDataCompare(fourCategory)
+plotDataCompare(fiveCategory)
+plotDataCompare(sixCategory)
+
+
+
+CompareCategoryMeans= c(mean(twoCategory$time), mean(threeCategory$time), mean(fourCategory$time), mean(fiveCategory$time), mean(sixCategory$time))
+CompareCatgeorySds = c(sd(twoCategory$time), sd(threeCategory$time), sd(fourCategory$time), sd(fiveCategory$time), sd(sixCategory$time))
+compareCategories = data.frame(CompareCategoryMeans, CompareCatgeorySds)
+colnames(compareCategories) = c("Means", "SDs")
+rownames(compareCategories) = c("twoCategory", "threeCategory", "fourCategory", "fiveCategory", "sixCategory")
+
+compareCategories$noOutlierMeans = c(mean(twoCategoryNoOutliers$time), mean(threeCategoryNoOutliers$time), mean(fourCategoryNoOutliers$time), mean(fiveCategoryNoOutliers$time), mean(sixCategoryNoOutliers$time))
+compareCategories$noOutlierSDs = c(sd(twoCategoryNoOutliers$time), sd(threeCategoryNoOutliers$time), sd(fourCategoryNoOutliers$time), sd(fiveCategoryNoOutliers$time), sd(sixCategoryNoOutliers$time))
+
+compareCategories
+
+
+
+# -- category number effect --
+
+
+
+
+plotDataCompare(allCategory, x = "categoryNumber", color = "categoryChar")
+
+dataSet = allCategory
+xAxis = "categoryNumber"
+colorVar = "categoryChar"
+dataName = deparse(substitute(allCategory))
+message(dataName)
+linearModel = lm(time ~ exp(categoryNumber), data = dataSet)
+plot1 = ggplot(dataSet, aes(x = categoryNumber, y = time)) +
+  geom_point(aes_string(color = colorVar))+
+  labs(title = dataName)+
+  geom_smooth(method = "gam", formula = (y ~ exp(x)), show.legend = F)+
+  annotate("text",
+           x = min(dataSet[,xAxis]+ ((max(dataSet[,xAxis]) - min(dataSet[,xAxis]))/4)), y = max(dataSet[, yAxis]), 
+           label = paste(
+             "y = x^", 
+             round(coef(linearModel)[2], 2), 
+             "+", 
+             round(coef(linearModel)[1], 2), 
+             "   R^2 = ", 
+             format(summary(linearModel)$r.squared, digits = 3)
+           )
+  )
+print(plot1)
+
+
+
+
+
+
+
+plotDataCompare(allCategory, color = "categoryChar")
+
+
 
 timeSpeciesCorrelation = function(dataSet){
   model = lm(time ~ speciesNum, data = dataSet)
@@ -66,15 +250,12 @@ timeCateogryCorrelation = function(dataSet){
   summary(model)
 }
 
-
 # -- plot with outliers -- 
-ggplot(compareData, aes(x = speciesNum, y = time, color = categoryNumber)) +
-  geom_point()
 
 timeSpeciesCorrelation(compareData) # no correlation between speceis number and time when mutliple categories 
 timeCateogryCorrelation(compareData)
 
-
+timeSpeciesCorrelation(twoCategory)
 
 
 
