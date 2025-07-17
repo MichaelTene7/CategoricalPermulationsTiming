b8e0f3beba032412b787273fbf8f29b904a8e00b
Michael Tene <110479175+MichaelTene7@users.noreply.github.com>
2025-07-17 16:13:26 -0400

    make double foreground dataframe instead of append, and time sumamrizer

--- Src/Loc/CombineBinaryTimes.R
+++ Src/Loc/CombineBinaryTimes.R
@@ -12,6 +12,64 @@ instances = 1:100
 relaxCombos = expand.grid(a = relaxLevels, b = relaxableTypes)
 runtypes = append(paste0(relaxCombos$b, relaxCombos$a), unrelaxableTypes)
 
+summaries = list()
+processTimes = function(timeObject, runType = NA, totaled = F){
+  
+  simulationTime = mean(timeObject[[1]], na.rm = T)
+  simulationDeviation = sd(timeObject[[1]], na.rm = T)
+  
+  pathTime = mean(timeObject[[2]], na.rm = T)
+  pathDeviation = sd(timeObject[[2]], na.rm = T)
+  
+  correlationTime = mean(timeObject[[3]], na.rm = T)
+  correlationDeviation = sd(timeObject[[3]], na.rm = T)
+  
+  agregatePvalTime = mean(timeObject[[5]], na.rm = T)
+  agregatePvalDeviation = sd(timeObject[[5]], na.rm = T)
+  permsPerAgregate = mean(timeObject[[4]], na.rm = T)
+  
+  pvalTime = agregatePvalTime/permsPerAgregate
+  pvalDeviation = agregatePvalDeviation/permsPerAgregate
+  
+  fulltimeMatirx = rbind(timeObject[[1]], timeObject[[2]], timeObject[[3]])
+  fulltimeValues = colSums(fulltimeMatirx, na.rm = T)
+  
+  subfullTime = mean(fulltimeValues, na.rm = T)
+  subfullDeviation = sd(fulltimeValues, na.rm = T)
+  
+  if(totaled){
+    simulationTime = simulationTime / permsPerAgregate
+    simulationDeviation = simulationDeviation / permsPerAgregate
+    
+    pathTime = pathTime / permsPerAgregate
+    pathDeviation = pathDeviation / permsPerAgregate
+    
+    correlationTime = correlationTime / permsPerAgregate
+    correlationDeviation = correlationDeviation / permsPerAgregate
+    
+    subfullTime = subfullTime / permsPerAgregate
+    subfullDeviation = subfullDeviation / permsPerAgregate
+    
+  }
+  
+  if(!is.na(pvalTime)){
+    fullTime = subfullTime + pvalTime    
+  }else{fullTime = subfullTime}
+  if(!is.na(pvalDeviation)){
+    fullDeviation = subfullDeviation + pvalDeviation
+  }else{fullDeviation = subfullDeviation}
+  
+  
+  outMessage = paste("\n-------- \n Run type:", runType, "| Totaled Type:", totaled, 
+                     "\n Time per permulation:", fullTime, "±", fullDeviation, 
+                     "\n Based on: simulation time", simulationTime, "±", simulationDeviation, "; Path time:", pathTime, "±", pathDeviation, "; \n Correlation time:", correlationTime, "±", correlationDeviation, "; pValue time:", pvalTime, "±", pvalDeviation)
+  cat(outMessage)
+  
+  output = list(outMessage, fullTime, fullDeviation, subfullTime, subfullDeviation, pvalTime, pvalDeviation, correlationTime, correlationDeviation, pathTime, pathDeviation, simulationTime, simulationDeviation)
+  names(output) = c("outMessage", "fullTime", "fullDeviation", "noPvalTime", "noPvalDeviation", "pvalTime", "pvalDeviation", "correlationTime", "correlationDeviation", "pathTime", "pathDeviation", "simulationTime", "simulationDeviation")
+  output
+}
+
 
 for(k in 1:length(runtypes)){
   
@@ -19,6 +77,10 @@ for(k in 1:length(runtypes)){
   
   message(k)
   message(currentRuntype)
+  
+  if(grepl("Categorical", currentRunType)){
+    categoricalRun = T
+  }else{categoricalRun = F}
   
   fileStarter = paste0(outputFolder, fileprefix, currentRuntype)
   combinedTimes = list()
@@ -67,11 +129,15 @@ for(k in 1:length(runtypes)){
       names(combinedForeground) = names(currentForeground)
     }else{
       for(j in 1:length(currentForeground)){
+        if(is.double(currentForeground[[j]])){
+          combinedForeground[[j]] = rbind(combinedForeground[[j]], currentForeground[[j]])
+        }
         combinedForeground[[j]] = append(combinedForeground[[j]], currentForeground[[j]])
         
       }
     }
   }
+  
   
   
   combineTimesName = paste0(currentRuntype, "CombinedTimes")
@@ -82,5 +148,13 @@ for(k in 1:length(runtypes)){
   
   assign(combineForegroundName, combinedForeground)
   saveRDS(combinedForeground, paste0(outputFolder, combineForegroundName, ".rds"))
+ 
   
+  typeSummary = processTimes(combinedTimes, currentRunType, categoricalRun)
+  summaries[[length(summaries)+1]] = typeSummary
+  names(summaries)[length(summaries)] = currentRunType 
 }
+
+summariesDataframe = do.call(rbind, lapply(summaries, as.data.frame))
+summariesFilename = paste0(outputFolder, fileprefix, "TimingSummaries.rds")
+saveRDS(summariesDataframe, summariesFilename)
